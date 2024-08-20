808447369951e759e15c869f82e9e4c8010ba8d0
Daun Kim <yennakim11@gmail.com>
2024-08-19 21:20:58 -0500

    created trip model

---
+++ takeoffapi/models/trip.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .user import User
+
+class Trip(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  trip_name = models.CharField(max_length=50)
+  origin = models.CharField(max_length=50)
+  destination = models.CharField(max_length=50)
+  start_date = models.CharField(max_length=50)
+  end_date = models.CharField(max_length=50)
