88ba62ec387499fd6cf76668734a4307a725541c
Daun Kim <yennakim11@gmail.com>
2024-08-20 17:07:52 -0500

    created packed item model and added 'city' property to lodging model

--- takeoffapi/models/__init__.py
+++ takeoffapi/models/__init__.py
@@ -3,3 +3,4 @@ from .trip import Trip
 from .traveler import Traveler
 from .trip_traveler import TripTraveler
 from .lodging import Lodging
+from .packed_item import PackedItem
--- takeoffapi/models/lodging.py
+++ takeoffapi/models/lodging.py
@@ -4,4 +4,5 @@ from .trip import Trip
 class Lodging(models.Model):
   trip_id = models.ForeignKey(Trip, on_delete=models.CASCADE)
   address = models.CharField(max_length=50)
+  city = models.CharField(max_length=50)
   length_of_stay = models.IntegerField()
