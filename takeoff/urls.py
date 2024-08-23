cc4d72c5e013692de7ffdc9f962a7c234c7f62d4
Daun Kim <yennakim11@gmail.com>
2024-08-22 22:10:22 -0500

    implemented read functionality for packed item

--- takeoff/urls.py
+++ takeoff/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from takeoffapi.views import UserView, TripView, LodgingView
+from takeoffapi.views import UserView, TripView, LodgingView, PackedItemView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'user', UserView, 'user')
 router.register(r'trip', TripView, 'trip')
 router.register(r'lodging', LodgingView, 'lodging')
+router.register(r'item', PackedItemView, 'packed_item')
 
 
 urlpatterns = [
--- takeoffapi/views/__init__.py
+++ takeoffapi/views/__init__.py
@@ -1,3 +1,4 @@
 from .user import UserView
 from .trip import TripView
 from .lodging import LodgingView
+from .packed_item import PackedItemView
