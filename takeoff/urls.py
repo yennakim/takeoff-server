187108e7e6d420aaa0e5342fe757506411016249
Daun Kim <yennakim11@gmail.com>
2024-08-22 22:23:23 -0500

    implemented read functionality for boarding pass

--- takeoff/urls.py
+++ takeoff/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from takeoffapi.views import UserView, TripView, LodgingView, PackedItemView
+from takeoffapi.views import UserView, TripView, LodgingView, PackedItemView, BoardingPassView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'user', UserView, 'user')
 router.register(r'trip', TripView, 'trip')
 router.register(r'lodging', LodgingView, 'lodging')
 router.register(r'items', PackedItemView, 'packed_item')
+router.register(r'boarding_pass', BoardingPassView, 'boarding_pass')
 
 
 urlpatterns = [
--- takeoffapi/views/__init__.py
+++ takeoffapi/views/__init__.py
@@ -2,3 +2,4 @@ from .user import UserView
 from .trip import TripView
 from .lodging import LodgingView
 from .packed_item import PackedItemView
+from .boarding_pass import BoardingPassView
