a6cd025eccb06c375c4032b91a49f82e9282302d
Daun Kim <yennakim11@gmail.com>
2024-08-26 22:29:13 -0500

    implemented trip traveler tests

---
+++ takeoffapi/tests/test_trip_traveler.py
@@ -0,0 +1,83 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from takeoffapi.models import TripTraveler, Trip, Traveler, User
+from takeoffapi.views.trip_traveler import TripTravelerSerializer
+
+class TripTravelerTests(APITestCase):
+
+    fixtures = ['user', 'traveler', 'trip', 'trip_traveler']
+    
+    def setUp(self):
+        self.user = User.objects.first()
+
+    def test_get_trip_traveler(self):
+        """Get Trip Traveler Test"""
+        trip_traveler = TripTraveler.objects.first()
+
+        url = f'/trip_traveler/{trip_traveler.id}'
+
+        response = self.client.get(url)
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        expected = TripTravelerSerializer(trip_traveler)
+
+        self.assertEqual(expected.data, response.data)
+
+    def test_list_trip_travelers(self):
+        """Test list trip travelers"""
+        url = '/trip_traveler'
+
+        response = self.client.get(url)
+        
+        all_trip_travelers = TripTraveler.objects.all()
+        expected = TripTravelerSerializer(all_trip_travelers, many=True)
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(expected.data, response.data)
+
+    def test_add_traveler(self):
+        """Test add traveler to trip"""
+        trip = Trip.objects.get(pk=1)
+        traveler = Traveler.objects.get(pk=2)
+
+        url = f'/trip/{trip.id}/add_traveler'
+
+        data = {
+            "trip_id": trip.id,
+            "traveler_id": traveler.id
+        }
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+
+        try: 
+          if response.status_code == status.HTTP_201_CREATED:
+            trip_traveler = TripTraveler.objects.get(trip=trip, traveler=traveler)
+            self.assertIsNotNone(trip_traveler)
+          else:
+            print(f"Error response: {response.data}")
+            
+        except TripTraveler.DoesNotExist:
+          print("TripTraveler object was not created.")
+
+    def test_remove_traveler(self):
+        """Test remove traveler from trip"""
+        trip_traveler = TripTraveler.objects.first()
+        trip = trip_traveler.trip
+        traveler = trip_traveler.traveler
+
+        url = f'/trip/{trip.id}/remove_traveler'
+
+        data = {
+            "traveler_id": traveler.id
+        }
+
+        response = self.client.delete(url, data, format='json')
+
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+
+        # Check if the TripTraveler object was deleted
+        self.assertFalse(TripTraveler.objects.filter(trip=trip, traveler=traveler).exists())
--- takeoffapi/views/trip_traveler.py
+++ takeoffapi/views/trip_traveler.py
@@ -11,7 +11,7 @@ class TripTravelerSerializer(serializers.ModelSerializer):
     depth = 1
 
 class TripTravelerView(ViewSet):
-  def retrieve(self, request, pk=None):
+  def retrieve(self, request, pk):
     try:
       trip_traveler = TripTraveler.objects.get(pk=pk)
       serializer = TripTravelerSerializer(trip_traveler)
@@ -19,7 +19,7 @@ class TripTravelerView(ViewSet):
     except Trip.DoesNotExist as ex:
       return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
     
-  def list(self, request):
-    trip_travelers = Trip.objects.all()
+  def list(self, request, pk=None):
+    trip_travelers = TripTraveler.objects.all()
     serializer = TripTravelerSerializer(trip_travelers, many=True)
     return Response(serializer.data)
