bce52b19a807d5368483abbff908ccd65fecaa23
Daun Kim <yennakim11@gmail.com>
2024-09-13 01:17:03 -0500

    updated packed item viewset to filter items by trip id

--- takeoffapi/views/packed_item.py
+++ takeoffapi/views/packed_item.py
@@ -22,7 +22,15 @@ class PackedItemView(ViewSet):
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
     def list(self, request):
-        items = PackedItem.objects.all()
+        trip_id = request.query_params.get('trip_id', None)
+        if trip_id is not None:
+            try:
+                trip = Trip.objects.get(id=trip_id)
+                items = PackedItem.objects.filter(trip=trip)
+            except Trip.DoesNotExist:
+                return Response({'message': 'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            items = PackedItem.objects.all()
         serializer = ItemSerializer(items, many=True)
         return Response(serializer.data)
 
